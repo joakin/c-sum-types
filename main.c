50623ada9a412e939ce4fabdfcbd0aeff92938f4
joakin <joaquin@chimeces.com>
2026-06-09 17:29:10 +0200

    Add `as` helpers to unwrap payload.

--- main.c
+++ main.c
@@ -9,15 +9,19 @@
 #include "sum_type.h"
 
 void dbg_msg(Msg *msg) {
-  switch (msg->tag) {
-  case Msg_Tag_Increment:
+  if (msg->kind == Msg_Kind_Set_Name) {
+    printf("checked name: %s\n", Msg_as_Set_Name(msg)->name);
+  }
+
+  switch (msg->kind) {
+  case Msg_Kind_Increment:
     printf("Increment\n");
     break;
-  case Msg_Tag_Decrement:
+  case Msg_Kind_Decrement:
     printf("Decrement\n");
     break;
-  case Msg_Tag_Set_Name:
-    printf("Set_Name %s\n", msg->Set_Name.name);
+  case Msg_Kind_Set_Name:
+    printf("Set_Name %s\n", Msg_as_Set_Name(msg)->name);
     break;
   }
 }
@@ -25,12 +29,11 @@ void dbg_msg(Msg *msg) {
 int main(int32_t argc, char *argv[]) {
   printf("Hello\n\n");
 
-  Msg msg = (Msg){.tag = Msg_Tag_Increment, .Increment = {}};
-  Msg msg2 = (Msg){.tag = Msg_Tag_Set_Name, .Set_Name = {.name = "Banana"}};
-
-  Msg msg5 = Sum(Msg, Set_Name, {.name = "Banana"});
-  Msg msg6 = Sum(Msg, Increment, {});
+  Msg msg = Sum(Msg, Increment, {});
+  Msg msg2 = Sum(Msg, Set_Name, {.name = "Banana"});
+  Msg msg3 = Sum(Msg, Decrement, {});
 
   dbg_msg(&msg);
   dbg_msg(&msg2);
+  dbg_msg(&msg3);
 }
--- sum_type.h
+++ sum_type.h
@@ -1,14 +1,21 @@
-#ifndef SUM_TYPE
-#define SUM_TYPE VARIANT(Test_Variant, )
-#define SUM_TYPE_NAME Test_Sum
+#ifndef SUM_TYPE_NAME
+#error "SUM_TYPE_NAME must be defined before including sum_type.h"
 #endif
+
+#ifndef SUM_TYPE
+#error "SUM_TYPE must be defined before including sum_type.h"
+#endif
+
+#include <assert.h>
 
 #define JOINK(a, sep, b) a##sep##b
 #define JOIN(a, sep, b) JOINK(a, sep, b)
+#define STRK(value) #value
+#define STR(value) STRK(value)
 
-#define VARIANT(name, fields) JOIN(SUM_TYPE_NAME, _Tag_, name),
+#define VARIANT(name, fields) JOIN(SUM_TYPE_NAME, _Kind_, name),
 
-typedef enum { SUM_TYPE } JOIN(SUM_TYPE_NAME, _, Tag);
+typedef enum { SUM_TYPE } JOIN(SUM_TYPE_NAME, _, Kind);
 
 #undef VARIANT
 #define FIELD(type, name) type name;
@@ -24,16 +31,29 @@ SUM_TYPE
 #define VARIANT(name, fields) JOIN(SUM_TYPE_NAME, _, name) name;
 
 typedef struct {
-  JOIN(SUM_TYPE_NAME, _, Tag) tag;
+  JOIN(SUM_TYPE_NAME, _, Kind) kind;
   union {
     SUM_TYPE
   };
 } SUM_TYPE_NAME;
 
+#undef VARIANT
+#define VARIANT(name, fields)                                                  \
+  static inline JOIN(SUM_TYPE_NAME, _, name) *                                 \
+      JOIN(SUM_TYPE_NAME, _as_, name)(SUM_TYPE_NAME *sum) {                    \
+    assert(sum->kind == JOIN(SUM_TYPE_NAME, _Kind_, name) &&                   \
+           STR(JOIN(SUM_TYPE_NAME, _as_, name)) " called with wrong kind");    \
+    return &sum->name;                                                         \
+  }
+
+SUM_TYPE
+
+#undef VARIANT
+
 #ifndef Sum
 #define Sum(type, variant, data)                                               \
   (type) {                                                                     \
-    .tag = JOIN(JOIN(type, _, Tag), _, variant),                               \
+    .kind = JOIN(type, _Kind_, variant),                                       \
     .variant = (JOIN(type, _, variant))data                                    \
   }
 #endif
@@ -42,3 +62,5 @@ typedef struct {
 #undef VARIANT
 #undef SUM_TYPE
 #undef SUM_TYPE_NAME
+#undef STR
+#undef STRK
